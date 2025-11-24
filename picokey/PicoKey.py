d6d44891429f07a79b2aef740feebaf41e9f1fe8
Pol Henarejos <pol.henarejos@cttc.es>
2025-11-24 17:05:21 +0100

    Add Observer to close the connection if needed.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- picokey/PicoKey.py
+++ picokey/PicoKey.py
@@ -17,10 +17,12 @@
  */
 """
 
-import sys
+from typing import Optional
+import threading
 from .APDU import APDUResponse
 from .SecureChannel import SecureChannel
 from .RescuePicoKey import RescuePicoKey
+from .RescueMonitor import RescueMonitor, RescueMonitorObserver
 from .PhyData import PhyData
 from .core import NamedIntEnum
 
@@ -36,38 +38,111 @@ class Product(NamedIntEnum):
     FIDO    = 2
     OPENPGP = 3
 
+class ConnectionType(NamedIntEnum):
+    UNKNOWN = 0
+    SMARTCARD = 1
+    RESCUE = 2
+
+class ConnectTimeout(Exception):
+    pass
+
+def connect_with_timeout(connection, timeout=2.0):
+    result = {}
+
+    def worker():
+        try:
+            connection.connect()
+            result["ok"] = True
+        except Exception as e:
+            result["error"] = e
+
+    t = threading.Thread(target=worker)
+    t.daemon = True
+    t.start()
+    t.join(timeout)
+
+    if t.is_alive():
+        raise ConnectTimeout("connection.connect() timed out")
+
+    if "error" in result:
+        raise result["error"]
+
+    return True
+
 class PicoKey:
+
     def __init__(self, slot=-1, force_rescue=False):
         self.__sc = None
-        if (force_rescue):
+        self.__card = None
+
+        if (not force_rescue):
+            from smartcard.System import readers
+            import smartcard.Exceptions
+            from smartcard.CardMonitoring import CardMonitor, CardObserver
+
+            class PicoCardObserver(CardObserver):
+                def __init__(self, device):
+                    self.__device = device
+
+                def update(self, observable, actions):
+                    (added, removed) = actions
+                    if added:
+                        pass
+                    if removed:
+                        self.__device.close()
+
+            def reader_has_card(reader):
+                try:
+                    connection = reader.createConnection()
+                    connect_with_timeout(connection, timeout=1.0)
+                    return connection
+                except smartcard.Exceptions.NoCardException:
+                    return None
+                return None
+
+            rdrs = readers()
+            if len(rdrs) > 0:
+                if (slot >= 0 and slot >= len(rdrs)):
+                    raise Exception('Slot number out of range')
+
+                if (slot >= 0 and slot < len(rdrs)):
+                    reader = rdrs[slot]
+                    connection = reader_has_card(reader)
+                    if (connection is None):
+                        raise Exception(f'No card in reader slot {slot}')
+                    self.__card = connection
+                else:
+                    for i, reader in enumerate(rdrs):
+                        reader = rdrs[i]
+                        connection = reader_has_card(reader)
+                        if (connection is None):
+                            continue
+                        self.__card = connection
+                        self.__connection_type = ConnectionType.SMARTCARD
+
+                        self.__monitor = CardMonitor()
+                        self.__observer = PicoCardObserver(self)
+                        self.__monitor.addObserver(self.__observer)
+                        break
+
+        if (self.__card is None):
+            class PicoRescueObserver(RescueMonitorObserver):
+                def __init__(self, device):
+                    self.__device = device
+
+                def update(self, actions):
+                    (connected, disconnected) = actions
+                    if connected:
+                        pass
+                    if disconnected:
+                        self.__device.close()
             try:
                 self.__card = RescuePicoKey()
+                self.__connection_type = ConnectionType.RESCUE
+                self.__observer = PicoRescueObserver(self.__card)
+                self.__monitor = RescueMonitor(device=self.__card, cls_callback=self.__observer)
             except Exception:
                 raise Exception('time-out: no card inserted')
-        else:
-            from smartcard.CardType import AnyCardType
-            from smartcard.CardRequest import CardRequest
-            cardtype = AnyCardType()
-            try:
-                # request card insertion
-                readers = None
-                if (slot >= 0):
-                    readers = CardRequest().getReaders()
-                    if (slot >= len(readers)):
-                        raise Exception('slot out of range')
-                    readers = [readers[slot]]
-                cardrequest = CardRequest(timeout=1, cardType=cardtype, readers=readers)
-                self.__card = cardrequest.waitforcard().connection
-
-                # connect to the card and perform a few transmits
-                self.__card.connect()
-
-            except Exception:
-                try:
-                    self.__card = RescuePicoKey()
-                except Exception:
-                    raise Exception('time-out: no card inserted')
-        resp, sw1, sw2 = self.select_applet(rescue=True)
         try:
             resp, sw1, sw2 = self.select_applet(rescue=True)
             if (sw1 == 0x90 and sw2 == 0x00):
@@ -79,7 +154,20 @@ class PicoKey:
             self.product = Product(Product.UNKNOWN)
             self.version = (0, 0)
 
+    @property
+    def device(self):
+        return self.__card
+
+    def has_device(self):
+        return self.__card is not None
+
+    @property
+    def connection_type(self):
+        return self.__connection_type
+
     def close(self):
+        if (not self.__card):
+            return
         if isinstance(self.__card, RescuePicoKey):
             self.__card.close()
         else:
@@ -87,11 +175,15 @@ class PicoKey:
             self.__card.release()
         self.__card = None
 
-    def transmit(self, apdu):
+    def transmit(self, apdu: list[int]):
+        if (not self.__card):
+            raise Exception('No device connected')
         response, sw1, sw2 = self.__card.transmit(apdu)
         return response, sw1, sw2
 
-    def send(self, command, cla=0x00, p1=0x00, p2=0x00, ne=None, data=None, codes=[]):
+    def send(self, command: int, cla: int = 0x00, p1: int =0x00, p2: int=0x00, ne : Optional[int] = None, data : Optional[list[int]] = None, codes : list[int] = []):
+        if (not self.__card):
+            raise Exception('No device connected')
         lc = []
         dataf = []
         if (data):
@@ -117,7 +209,10 @@ class PicoKey:
             response, sw1, sw2 = self.__card.transmit(apdu)
         except Exception:
             self.__card.reconnect()
-            response, sw1, sw2 = self.__card.transmit(apdu)
+            try:
+                response, sw1, sw2 = self.__card.transmit(apdu)
+            except Exception as e:
+                raise Exception("APDU transmission error after reconnect: " + str(e))
 
         code = (sw1<<8|sw2)
         if (sw1 != 0x90):
@@ -157,24 +252,24 @@ class PicoKey:
 
         return bytes(response), sw1, sw2
 
-    def open_secure_channel(self, shared, nonce, token, pbkeyBytes):
+    def open_secure_channel(self, shared: bytes, nonce: bytes, token: bytes, pbkeyBytes: bytes):
         sc = SecureChannel(shared=shared, nonce=nonce)
         res = sc.verify_token(token, pbkeyBytes)
         if (not res):
             raise Exception('Secure Channel token verification failed')
         self.__sc = sc
 
-    def select_applet(self, rescue=False):
+    def select_applet(self, rescue : bool = False):
         if (rescue):
             return self.transmit([0x00, 0xA4, 0x04, 0x04, 0x08, 0xA0, 0x58, 0x3F, 0xC1, 0x9B, 0x7E, 0x4F, 0x21, 0x00])
         return self.transmit([0x00, 0xA4, 0x04, 0x00, 0x0B, 0xE8, 0x2B, 0x06, 0x01, 0x04, 0x01, 0x81, 0xC3, 0x1F, 0x02, 0x01, 0x00])
 
-    def phy(self, data=None):
+    def phy(self, data : Optional[list[int]] = None):
         if (data is None):
             try:
                 resp, sw = self.send(0x1E, cla=0x80, p1=0x00, ne=256)
                 return PhyData.parse(resp)
-            except APDUResponse:
+            except Exception:
                 pass
             return None
         else:
@@ -206,6 +301,6 @@ class PicoKey:
             'boot_key': resp[2]
         }
 
-    def secure_boot(self, bootkey_index=0, lock=False):
+    def secure_boot(self, bootkey_index: int = 0, lock: bool = False):
         data = bytes([bootkey_index & 0xFF, 1 if lock else 0])
         self.send(0x1C, cla=0x80, p1=0x02, data=data)
